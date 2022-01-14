36af4658fb9d16b893ef01d6861c52711d5056ba
Anthony Quick <quickanthony314@gmail.com>
2022-01-13 19:45:50 -0600

    new commit

---
+++ src/com/company/Menu.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Date;
+public class Menu {
+
+	 private ArrayList appetizers = new ArrayList<>();
+     private ArrayList mains = new ArrayList<>();
+     private ArrayList desserts = new ArrayList<>();
+     private Date lastuUpdated;
+
+
+    public Menu(ArrayList appetizers, ArrayList mains, ArrayList desserts, Date lastuUpdated) {
+        this.appetizers = appetizers;
+        this.mains = mains;
+        this.desserts = desserts;
+        this.lastuUpdated = lastuUpdated;
+    }
+
+
+    public ArrayList getAppetizers() {
+        return appetizers;
+    }
+
+    public void setAppetizers(ArrayList appetizers) {
+        this.appetizers = appetizers;
+    }
+
+    public ArrayList getMains() {
+        return mains;
+    }
+
+    public void setMains(ArrayList mains) {
+        this.mains = mains;
+    }
+
+    public ArrayList getDesserts() {
+        return desserts;
+    }
+
+    public void setDesserts(ArrayList desserts) {
+        this.desserts = desserts;
+    }
+
+    public Date getLastuUpdated() {
+        return lastuUpdated;
+    }
+
+    public void setLastuUpdated(Date lastuUpdated) {
+        this.lastuUpdated = lastuUpdated;
+    }
+}
---
+++ src/com/company/Menuitem.java
@@ -0,0 +1,70 @@
+package com.company;
+import java.util.Date;
+
+
+public class Menuitem {
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+    private Date dateAdded;
+    private boolean newNess;
+
+
+    public Menuitem(String name, double price, String description, String category, Date dateAdded) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.dateAdded = dateAdded;
+        this.newNess = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public boolean isNewNess() {
+        return newNess;
+    }
+
+    public void setNewNess(boolean newNess) {
+        this.newNess = newNess;
+    }
+}
